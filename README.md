8d424d0fd5b1bed9db99d08d500f1963dd3c32f1
procodingclass@whitehatjr.com <procodingclass@whitehatjr.com>
2021-09-02 12:57:23 +0530

    Revamp version 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tablet-SPCK-PRO-C2-Student-Activity
+Student Activity
---
+++ sketch.js
@@ -0,0 +1,35 @@
+var trex, trexRunning;
+
+function preload() {
+  // Loading trex animation
+  trexRunning = loadAnimation("trex1.png", "trex2.png", "trex3.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  // create trex Sprite & add animation to it
+  // NOTE: checkout Step 1 image inside screenshots folder
+
+
+
+  // adding scale and position to trex
+  // NOTE: checkout Step 2 image inside screenshots folder
+
+
+}
+
+function draw() {
+  //set background color
+  background("white");
+
+  //jump when space key is pressed
+  // NOTE: checkout Step 4 image inside screenshots folder
+
+
+  // Add gravity to trex Sprite
+  // NOTE: checkout Step 3 image inside screenshots folder
+
+
+  drawSprites();
+}
